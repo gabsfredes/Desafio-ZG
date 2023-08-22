628afaef2034dd2b67fd1a729d43962704973616
Gabriel Fredes <gabriel.sfredes@gmail.com>
2023-08-21 23:04:42 -0300

    Final commit
    Ajustes finais

--- patinhos.py
+++ patinhos.py
@@ -16,19 +16,32 @@ def convertidoMaiusculo(patinhos):
     patinhosExtenso = converte.capitalize()
     return patinhosExtenso
 
-def unidadesPatinhos(patinhos):
-    if patinhos == 1:
-        unidades = "patinho voltou"
-    else: 
-        unidades = "patinhos voltaram"   
+def unidadesPatinhos(patinhos, onde):
+    if onde == "letra":
+        if patinhos == 1:
+            unidades = "patinho voltou"
+        else: 
+            unidades = "patinhos voltaram"   
+    elif onde == "final":
+        if patinhos == 1:
+            unidades = "o"
+        else: 
+            unidades = "os"
+    
     return unidades
             
 def Letra(patinhos):
     while patinhos >= 1:
+        unidades = unidadesPatinhos(patinhos-1, "letra")
         if patinhos > 1:
-            return f"{convertidoMaiusculo(patinhos)} patinhos foram passear\nAlém das montanhas\nPara brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas só {convertidoMaiusculo(patinhos-1)} {unidadesPatinhos(patinhos-1)} de lá.\n\n"
+            return f"\n{convertidoMaiusculo(patinhos)} patinhos foram passear\nAlém das montanhas\nPara brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas só {convertidoMaiusculo(patinhos-1)} {unidades} de lá.\n"
         elif patinhos == 1:
-            return f"{convertidoMaiusculo(patinhos)} patinho foi passear\nAlém das montanhas\nPara brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas nenhum patinho voltou de lá.\n\n"
+            return f"\n{convertidoMaiusculo(patinhos)} patinho foi passear\nAlém das montanhas\nPara brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas nenhum patinho voltou de lá.\n"
+
+def letraFinal(patinhos):
+    unidadesUm = unidadesPatinhos(patinhos, "letra")
+    unidadesDois = unidadesPatinhos(patinhos, "final")
+    return f"Poxa, a mamãe patinha ficou tão triste naquele dia\nAonde será que estavam os seus filhotinhos?\nMas essa história vai ter um final feliz\nSabe por quê?\n\nA mamãe patinha foi procurar\nAlém das montanhas, na beira do mar\nA mamãe gritou: Quá, quá, quá, quá!\nE {unidadesDois} {patinhos} {unidadesUm} de lá\n"
 
 def main():
     while True:
@@ -44,6 +57,9 @@ def main():
     for patinho in range(patinhos, 0, -1):
         letra = Letra(patinho)
         print(letra)
+        if patinho == 1:
+            final = letraFinal(patinhos)
+            print(final)
     
    
 if __name__ == "__main__":
